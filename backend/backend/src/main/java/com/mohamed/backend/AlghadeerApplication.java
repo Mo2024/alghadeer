593a2d910dc7542f1b8f3b1d45e359a4df268f91
Mo2024 <mohdosama2025@gmail.com>
2025-06-15 04:56:52 +0300

    Initial Commit

---
+++ backend/backend/src/main/java/com/mohamed/backend/AlghadeerApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlghadeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlghadeerApplication.class, args);
+	}
+
+}
