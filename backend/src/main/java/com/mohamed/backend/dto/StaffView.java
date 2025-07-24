aeb19b4d0fbb6c2f8c913c4930dd4a06a7185266
Mo2024 <mohdosama2025@gmail.com>
2025-07-24 21:10:16 +0300

    added permissions list in the getStaff view

--- backend/src/main/java/com/mohamed/backend/dto/StaffView.java
+++ backend/src/main/java/com/mohamed/backend/dto/StaffView.java
@@ -1,6 +1,11 @@
 package com.mohamed.backend.dto;
 
+import com.mohamed.backend.model.user.StaffPermission;
+import java.util.List;
+
+
 public interface StaffView {
     Long getId();
     String getName();
+    List<StaffPermission> getPermissions();
 }
