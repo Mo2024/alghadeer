af1b2d2f853cf847ad1433ac4c9b14b682252380
Mo2024 <mohdosama2030@gmail.com>
2025-08-16 06:23:49 +0300

    fixing more stuff

--- frontend/src/app/staff/instructor/assigned-classes/assignments/assignment-details/assignment-details.component.ts
+++ frontend/src/app/staff/instructor/assigned-classes/assignments/assignment-details/assignment-details.component.ts
@@ -27,7 +27,6 @@ export class AssignmentDetailsComponent {
     'يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو',
     'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر'
   ];
-
   toArabicNumbers(str: string | number): string {
     const arabicNumbers = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
     return String(str).replace(/\d/g, d => arabicNumbers[+d]);
@@ -42,12 +41,21 @@ export class AssignmentDetailsComponent {
     const month = this.arabicMonths[date.getMonth()];
     const year = this.toArabicNumbers(date.getFullYear());
 
-    const hours = this.toArabicNumbers(date.getHours().toString().padStart(2, '0'));
-    const minutes = this.toArabicNumbers(date.getMinutes().toString().padStart(2, '0'));
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    let period = hours < 12 ? 'صباحاً' : 'مساءً';
+    if (hours === 0) hours = 12;
+    else if (hours > 12) hours -= 12;
+
+    const arabicHours = this.toArabicNumbers(hours);
+    const arabicMinutes = minutes > 0 ? ':' + this.toArabicNumbers(minutes.toString().padStart(2, '0')) : '';
 
     const dayName = this.arabicDays[date.getDay()];
 
-    return `${dayName} ${day} ${month} ${year}, ${hours}:${minutes}`;
+    return `${dayName} ${day} ${month} ${year}<br>${arabicHours}${arabicMinutes} ${period}`;
   }
+
+
 
 }
